cab2f3a4dd2762078212547fbecbfc43ff553e12
YinXingZhu <yinxingzhu72@gmail.com>
2026-08-13 11:26:41 +0800

    add learning goal decision logic

--- projects/first_python/main.py
+++ projects/first_python/main.py
@@ -2,4 +2,7 @@ user_name = input("请输入你的名字：")
 
 print("你好，" + user_name + "！欢迎来到 AI Tutor。")
 learning_goal = input("今天想学习什么？")
-print("好的，我们来学习 " + learning_goal + "！")
+if learning_goal == "Python":
+    print("很好！Python 是编程入门的好选择，我们先学习变量和输入输出。")
+else:
+    print("好的，我们来学习 " + learning_goal + "！")
